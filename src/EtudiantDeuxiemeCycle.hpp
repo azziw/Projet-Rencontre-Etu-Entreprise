ce4d5a3200016b1b2e222288cce0c3895de4583e
azziw <62752916+azziw@users.noreply.github.com>
2024-12-30 18:38:14 +0100

    Add files via upload

---
+++ src/EtudiantDeuxiemeCycle.hpp
@@ -0,0 +1,19 @@
+# include <iostream>
+#include <string>
+#include <vector>
+
+#include "etudiant.hpp"
+
+using namespace std;
+
+
+class EtudiantDeuxiemeCycle : public Etudiant {
+    string NomDisciplinePrincipale;
+
+    public:
+        // Constructeur
+        EtudiantDeuxiemeCycle(string NomDiscipline){
+            NomDisciplinePrincipale = NomDiscipline;
+        }
+
+}
