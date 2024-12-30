01e4fd87d49ba464b966b8b6a964e44d23e855ac
azziw <62752916+azziw@users.noreply.github.com>
2024-12-30 18:35:36 +0100

    Add files via upload

---
+++ src/etudiant.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+#include <string>
+#include <vector>
+
+#include "etudiant.hpp"
+#include "ExperiencePro.hpp"
+
+using namespace std;
+
+// Ajout d'un diplome
+void Etudiant::addDiplome(Diplome D){
+    listeDiplomes.push_back(D);
+    D->listeDiplomes.push_back(this); 
+}
+
+// Ajout d'une Experience
+void Etudiant::addExperience(ExperiencePro E){
+    listeExperience.push_back(E);
+    E->listeExperience.push_back(this); 
+}
+
+// Ajout d'un Rendez-Vous
+void Etudiant::addRendezVous(RendezVous R){
+    rendezVous.push_back(R);
+    R->rendezVous.push_back(this); 
+}
+
+s
